4de5de8b5bfb1dfbfb81014b4999c2066e53b7e8
Hannah Jayne Knight <hannah@tinpot.co.uk>
2021-10-19 10:17:51 +0100

    tute 0, Q1, asnwers

--- tutorial0.m
+++ tutorial0.m
@@ -1,6 +1,6 @@
 clear all;
 
-% 1. Program to compute the first and second derivatives for 𝐱 = [0, 1, 2, 3, 4, 5,6,7]
+% Q1 Program to compute the first and second derivatives for 𝐱 = [0, 1, 2, 3, 4, 5,6,7]
 
 x = [0, 1, 2, 3, 4, 5, 6, 7];
 f = (1/100).*x.*(x-1).*(x-4).*(x-5).*(x-7);
@@ -8,7 +8,7 @@ f = (1/100).*x.*(x-1).*(x-4).*(x-5).*(x-7);
 diff1 = diff(f);
 diff2 = diff(f, 2);
 
-% ANSWER - using approximations
+% Q1 ANSWER - using approximations
 
 dx = 0.001; % delta x
 fx = fun(x); % function values at x
@@ -18,8 +18,11 @@ dfx = (fxdx-fx)/dx; % first derivative at x values
 dfxdx = (fx2dx-fxdx)/dx; % first derivative at x+dx
 ddfx = (dfxdx-dfx)/dx; % second derivative at x
 
+
 % 2. 
 
+
+% FUNCTIONS
 function y=fun(x)
 y = 1/100*x.*(x-1).*(x-4).*(x-5).*(x-7);
 end
