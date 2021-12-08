faec8ff9ac1ec76a1613c95b59dea94b5623fbdf
Hannah Jayne Knight <hannah@tinpot.co.uk>
2021-12-08 22:25:24 +0000

    up to the 8th dec

--- coursework/generatingsamples.m
+++ coursework/generatingsamples.m
@@ -2,10 +2,10 @@ clear all;
 
 % sampling
 lhs = lhsdesign(30, 4, 'Iterations', 20);
-R_min = 0; R_max = 0; %revolutions of the helix
-rh_min = 0; rh_max = 0; %radius of the helix
-ro_min = 0; ro_max = 0; %outer radius of the water pipe
-p_min = 0; p_max = 0; %pitch of the helix
+R_min = 1; R_max = 10; %revolutions of the helix
+rh_min = 2; rh_max = 5; %radius of the helix
+ro_min = 0.5; ro_max = 2; %outer radius of the water pipe
+p_min = 0.5; p_max = 20; %pitch of the helix
 
 R_range = R_max - R_min; %ranges
 rh_range = rh_max - rh_min;
@@ -16,4 +16,13 @@ lhs_R = R_min + lhs(:,1)*R_range;
 lhs_rh = rh_min + lhs(:,1)*rh_range;
 lhs_ro = ro_min + lhs(:,1)*ro_range;
 lhs_p = p_min + lhs(:,1)*p_range;
-lhs_var = [lhs_R lhs_rh lhs_ro lhs_p];
+
+%getting results from the equation
+
+%constants
+k = 237;
+delta_T = 107;
+squareroot = sqrt( (lhs_p.^2) + ( 2*pi .* lhs_rh) .^2);
+lhs_Q = ( 2*pi*k*lhs_R.*squareroot*delta_T)./ log(lhs_ro./(lhs_ro-1));
+lhs_var = [lhs_R lhs_rh lhs_ro lhs_p lhs_Q];
+writematrix(lhs_var, 'lhs_samples.csv');
