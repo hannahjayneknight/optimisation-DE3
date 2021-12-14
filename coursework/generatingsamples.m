91df0bfc99857d0c26e5250e5aacffe1f2a89f4c
Hannah Jayne Knight <hannah@tinpot.co.uk>
2021-12-14 16:32:00 +0000

    adding Q as a constraint to optimisation problem

--- coursework/generatingsamples.m
+++ coursework/generatingsamples.m
@@ -1,7 +1,7 @@
 clear all;
 
 % sampling
-lhs = lhsdesign(30, 4, 'Iterations', 20); %<--- increase this to 70
+lhs = lhsdesign(70, 4, 'Iterations', 20); %<--- increase this to 70
 R_min = 2e-03; R_max = 25e-03; %revolutions of the helix
 rh_min = 17.5e-03; rh_max = 23.5e-03; %radius of the helix
 ro_min = 1.5e-03; ro_max = 7.5e-03; %outer radius of the water pipe
@@ -26,4 +26,4 @@ delta_T = 107;
 squareroot = sqrt( (lhs_p.^2) + ( 2*pi .* lhs_rh) .^2);
 lhs_Q = ( 2*pi*k*lhs_R.*squareroot*delta_T)./ log(lhs_ro./(lhs_ri));
 lhs_var = [lhs_R lhs_rh lhs_ro lhs_p lhs_Q];
-writematrix(lhs_var, 'lhs_samples.csv');
+writematrix(lhs_var, 'lhs_samples2.csv');
