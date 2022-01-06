c2e67e32f83b5f29315022817250b0f6512f1374
Hannah Jayne Knight <hannah@tinpot.co.uk>
2022-01-06 12:43:51 +0000

    MATLAB for tutorial 4

---
+++ tutorial4/T4_q3.m
@@ -0,0 +1,39 @@
+function T4_q3(x0)
+
+
+x_1 = -20:0.1:20;
+x_2 = -20:0.1:20;
+[x11, x22] = meshgrid(x_1,x_2);
+zz = f(x11, x22);
+contour3(x11,x22,zz, 50)
+hold on
+mesh(x11, x22, zz)
+xlabel('x_1')
+ylabel('x_2')
+colorbar
+
+
+for i = 1:10
+    f0 = f(x0(1),x0(2)); 
+    [g,h] = fprime(x0(1),x0(2));
+    s = -h*g;
+    x1 = x0+s;
+    f1 = f(x1(1),x1(2));
+    delta = (x0-x1)'*(x0-x1);
+    plot3([x0(1) x1(1)],[x0(2),x1(2)], [f0,f1],'r-*');
+    x0=x1;
+    fprintf('Iter %d: x1=%f, x2=%f, f=%f\n', i, x1(1), x1(2), f1)
+    if delta<=1e-9
+        break
+    end
+end
+
+function zz = f(x11, x22)
+zz = 0.5.*x11.^2+3.*x11+0.5.*x22.^2-7.*x22;
+
+function [g, h] = fprime(x1, x2)
+dfdx1 = x1+3;
+dfdx2 = x2-7;
+g = [dfdx1;dfdx2];
+h = inv([1 0; 0 1]);
+
---
+++ tutorial4/T4_q4.m
@@ -0,0 +1,21 @@
+clc
+clear
+close all
+
+x1 = -10:0.1:10;
+x2 = -10:0.1:10;
+[x11, x22] = meshgrid(x1, x2);
+
+zz = x11.^2-3.*x11-x22+x11.*x22;
+contour3(x11, x22, zz, 100);
+xlabel('x_1')
+ylabel('x_2')
+zlabel('f(x)')
+hold on
+plot3(1, 1, 1^2-3*1-1+1*1, 'rs');
+colorbar;
+plot(ones(length(x1)), x2,'k')
+plot(x1,2-x1,'k')
+plot(x1,x1,'r')
+xlim([-10,10])
+ylim([-10,10])
