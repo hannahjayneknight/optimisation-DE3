2d1d9aa3c8cd526bf3b68dcd589c144c91074428
Hannah Jayne Knight <hannah@tinpot.co.uk>
2022-01-06 13:10:29 +0000

    adding my comments to q4

--- tutorial4/T4_q4.m
+++ tutorial4/T4_q4.m
@@ -1,13 +1,16 @@
+% from plotting the function, we can see the saddle shape of the function
+% very clearly
+
 clc
 clear
 close all
 
-x1 = -10:0.1:10;
+x1 = -10:0.1:10; % generating sample data
 x2 = -10:0.1:10;
 [x11, x22] = meshgrid(x1, x2);
 
 zz = x11.^2-3.*x11-x22+x11.*x22;
-contour3(x11, x22, zz, 100);
+contour3(x11, x22, zz, 100); % plotting the function
 xlabel('x_1')
 ylabel('x_2')
 zlabel('f(x)')
